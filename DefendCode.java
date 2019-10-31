feb97dd2f385b1175060c6f26e2baf2dd817f58e
ericshervey <ericbshervey@gmail.com>
2019-10-30 19:31:13 -0700

    Add files via upload

---
+++ DefendCode.java
@@ -0,0 +1,45 @@
+package defendCode;
+
+public class DefendCode {
+
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	private static void readName()
+	{
+		
+	}
+	
+	private static void readInts()
+	{
+		
+	}
+	
+	private static void getInputFile()
+	{
+		
+	}
+	
+	private static void getOutputFile()
+	{
+		
+	}
+	
+	private static void writeName()
+	{
+		
+	}
+	
+	private static void writeSum()
+	{
+		
+	}
+	
+	private static void writeContents()
+	{
+		
+	}
+}
