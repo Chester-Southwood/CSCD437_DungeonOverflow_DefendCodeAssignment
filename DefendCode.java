9922c041f39b74469a9d28f9fde2051fdb10ffa5
ericshervey <ericbshervey@gmail.com>
2019-10-31 00:18:46 -0700

    Added getName function and some basic validation

--- DefendCode.java
+++ DefendCode.java
@@ -5,12 +5,36 @@ public class DefendCode {
 	
 	public static void main(String[] args)
 	{
+		String fName = "";
+		String lName = "";
+		
+		readName(fName, lName);
 		
 	}
 	
-	private static void readName()
+	private static void readName(String fName, String lName)
 	{
+		Scanner kIn = new Scanner(System.in);
 		
+		System.out.println("Input First Name with length of at most 20 characters");
+		fName = kIn.nextLine();
+		
+		while(fName.length() > 20)
+		{
+			System.out.println("Input was too long. Reenter first name with at most 20 characters");
+			fName = kIn.nextLine();
+		}
+		
+		System.out.println("Input Last Name with length of at most 20 characters");
+		lName = kIn.nextLine();
+		
+		while(lName.length() > 30)
+		{
+			System.out.println("Input was too long. Reenter first name with at most 20 characters");
+			lName = kIn.nextLine();
+		}
+		
+		//Here we should do some RegEx to make sure names are formatted right
 	}
 	
 	private static void readInts()
