0d58940197aaa5bb5bb1c2da6f949c746de3e42e
ericshervey <ericbshervey@gmail.com>
2019-11-03 18:09:28 -0800

    Update DefendCode.java

--- DefendCode.java
+++ DefendCode.java
@@ -1,5 +1,10 @@
 package defendCode;
 
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DefendCode {
 
 	
@@ -16,25 +21,32 @@ public class DefendCode {
 	{
 		Scanner kIn = new Scanner(System.in);
 		
-		System.out.println("Input First Name with length of at most 20 characters");
+		System.out.println("Input First Name with length of at most 20 characters and comprised of only upper or lower case alphabetic characters");
 		fName = kIn.nextLine();
 		
-		while(fName.length() > 20)
+		Pattern p = Pattern.compile("^[a-zA-Z]{1,20}$");
+		Matcher m = p.matcher(fName);
+		
+		System.out.println(m.matches());
+		
+		while(!m.matches())
 		{
-			System.out.println("Input was too long. Reenter first name with at most 20 characters");
+			System.out.println("Input was invalid. You must have between 1 and 20 upper or lower case alphabetic characters for first name");
 			fName = kIn.nextLine();
 		}
 		
-		System.out.println("Input Last Name with length of at most 20 characters");
+		System.out.println("Input Last Name with length of at most 30 characters and comprised of only upper or lower case alphabetic characters");
 		lName = kIn.nextLine();
 		
-		while(lName.length() > 30)
+		p = Pattern.compile("^[a-zA-Z]{1,30}$");
+		Matcher m = p.matcher(fName);
+		
+		while(!m.matches())
 		{
-			System.out.println("Input was too long. Reenter first name with at most 20 characters");
+			System.out.println("Input was invalid. You must have between 1 and 30 upper or lower case alphabetic characters for last name");
 			lName = kIn.nextLine();
 		}
 		
-		//Here we should do some RegEx to make sure names are formatted right
 	}
 	
 	private static void readInts()
@@ -42,8 +54,25 @@ public class DefendCode {
 		
 	}
 	
-	private static void getInputFile()
+	private static file getInputFile()
 	{
+		Scanner kIn = new Scanner(System.in);
+		System.out.println("Input the input file name. Only alphabetic characters may be used. Only .txt files may be used. File must exist in same directory the program is in.");
+		
+		File inFile = new File(kIn.hasNextLine());
+		Pattern pat = Pattern.compile("^\\w+.txt$");
+		Matcher m = pat.matcher(inFile.getPath());
+		
+		while(!inFile.exists() && !m.matches())
+		{
+			System.out.println("File does not exist. Re-enter with a existing file. If no file exists please create one.");
+			
+			//Do RegEx to check for valid file path?
+			
+			File inFile = new File(kIn.hasNextLine());
+		}
+		
+		return inFile;
 		
 	}
 	
